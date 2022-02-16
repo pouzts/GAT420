28c11007ea8e3e74f541f2a2b981697cfbb55e00
Peter Ouzts <81652748+pouzts@users.noreply.github.com>
2022-02-16 11:48:42 -0700

    Worked more in Utility Agent - 16 February 2022.
    Added Need, FixedCamera, and UtilityObject classes.  Worked in the UtilityAgent Class.

--- Assets/Scripts/UtilityAgent/UtilityAgent.cs
+++ Assets/Scripts/UtilityAgent/UtilityAgent.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class UtilityAgent : Agent
 {
-    // Start is called before the first frame update
+    Need[] needs;
+
     void Start()
     {
-        
+        needs = GetComponentsInChildren<Need>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnGUI()
+    {
+        Vector2 screen = Camera.main.WorldToScreenPoint(transform.position);
+
+        GUI.color = Color.black;
+        int offset = 0;
+        foreach (var need in needs)
+        {
+            GUI.Label(new Rect(screen.x + 20, Screen.height - screen.y - offset, 300, 20), need.type.ToString() + ": " + need.value);
+            offset += 20;
+        }
+        //GUI.Label(new Rect(screen.x + 20, Screen.height - screen.y - offset, 300, 20), mood.ToString());
     }
 }
